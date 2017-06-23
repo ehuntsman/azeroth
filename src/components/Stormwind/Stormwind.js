db4e6af456679fa72a90100dde18684b41a0aae8
Elizabeth <ehuntsman@wavetronix.com>
2017-06-23 11:27:30 -0600

    stormwind gradient

--- src/components/Stormwind/Stormwind.js
+++ src/components/Stormwind/Stormwind.js
@@ -1,21 +1,102 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
+import {Link} from 'react-router-dom';
 
 import shopping from './shopping.png';
 import fishing from './fishing.png';
 import eating from './eating.png';
+import home from './home.png';
+import swgreet from './SWgreetings.svg';
 
 class Stormwind extends Component {
     render() {
+        $(document).ready(function(){
+            $("#home").hover(
+                function(){
+                    $("#home img").animate({
+                        height: '+=5px',
+                        width: '+=5px'
+                    });
+                }, function(){
+                    $("#home img").animate({
+                        height: '-=5px',
+                        width: '-=5px'
+                    });
+                }
+            ),
+            $("#shop").hover(
+                function(){
+                    $("#shop img").animate({
+                        height: '+=5px',
+                        width: '+=5px'
+                    });
+                }, function(){
+                    $("#shop img").animate({
+                        height: '-=5px',
+                        width: '-=5px'
+                    });
+                }
+            ),
+            $("#fish").hover(
+                function(){
+                    $("#fish img").animate({
+                        height: '+=5px',
+                        width: '+=5px'
+                    });
+                }, function(){
+                    $("#fish img").animate({
+                        height: '-=5px',
+                        width: '-=5px'
+                    });
+                }
+            ),
+            $("#eat").hover(
+                function(){
+                    $("#eat img").animate({
+                        height: '+=5px',
+                        width: '+=5px'
+                    });
+                }, function(){
+                    $("#eat img").animate({
+                        height: '-=5px',
+                        width: '-=5px'
+                    });
+                }
+            )
+        })
         return (
             <div className="stormwind-container">
                 <div className="para-one">
-                    <h1>stormwind baby</h1>
+                    <img src={swgreet} />
+                </div>
+                <div className="para-two">
+                    <p>Home to the king blah blah blah</p>
+                </div>
+                <div className="para-three">
+                </div>
+                <div className="para-four">
+                    <p>Home to the king blah blah blah</p>
+                </div>
+                <div className="para-five">
                 </div>
                 <section className="stormwind-nav">
                     <ul>
-                        <li><img src={shopping} alt="shopping"/></li>
-                        <li><img src={fishing} alt="fishing"/></li>
-                        <li><img src={eating} alt="eating"/></li>
+                        <li id="home"><Link to='/'>
+                            <img src={home} alt="home"/>
+                            <p>home</p>
+                        </Link></li>
+                        <li id="shop">
+                            <img src={shopping} alt="shopping"/>
+                            <p>shopping</p>
+                        </li>
+                        <li id="fish">
+                            <img src={fishing} alt="fishing"/>
+                            <p>fishing</p>
+                        </li>
+                        <li id="eat">
+                            <img src={eating} alt="eating"/>
+                            <p>food</p>
+                        </li>
                     </ul>
                 </section>
             </div>
